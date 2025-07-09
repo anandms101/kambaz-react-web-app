d1423b568efab35a17263f719ee2ef07bdecd061
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2025-07-08 21:52:59 -0400

    Navigation fixed, name added on landing page

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -8,6 +8,7 @@ export default function Labs() {
     return (
         <div id="wd-labs">
             <h1>Labs</h1>
+            <h2>Anand Mohan Singh - Web Development summer - 2 2025</h2>
             <TOC />
             <Routes>
                 <Route path="/" element={<Navigate to="Lab1" />} />
