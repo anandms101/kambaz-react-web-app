0ca2bf2d7cc4075775727a6996644efa24f5a2f9
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2025-07-15 18:49:14 -0400

    Card image changed, account styling added

--- src/Kambaz/Account/Signup.tsx
+++ src/Kambaz/Account/Signup.tsx
@@ -2,18 +2,20 @@ import { Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 export default function Signup() {
   return (
-    <div id="wd-signin-screen">
-      <h1>Sign up</h1>
-      <Form.Control id="wd-username"
-        placeholder="username"
-        className="mb-2" />
-      <Form.Control id="wd-password"
-        placeholder="password" type="password"
-        className="mb-2" />
-      <Link id="wd-signin-btn"
-        to="/Kambaz/Account/Profile"
-        className="btn btn-primary w-100 mb-2">
-        Sign up </Link>
-      <Link id="wd-signup-link" to="/Kambaz/Account/Signin">Sign in</Link>
+    <div className="d-flex justify-content-center align-items-center" style={{ minHeight: "80vh" }}>
+      <div id="wd-signin-screen" style={{ maxWidth: 400, width: "100%" }}>
+        <h1>Sign up</h1>
+        <Form.Control id="wd-username"
+          placeholder="username"
+          className="mb-2" />
+        <Form.Control id="wd-password"
+          placeholder="password" type="password"
+          className="mb-2" />
+        <Link id="wd-signin-btn"
+          to="/Kambaz/Account/Profile"
+          className="btn btn-primary w-100 mb-2">
+          Sign up </Link>
+        <Link id="wd-signup-link" to="/Kambaz/Account/Signin">Sign in</Link>
+      </div>
     </div>);
 }
--- src/Labs/Lab2/BootstrapNavigation.tsx
+++ src/Labs/Lab2/BootstrapNavigation.tsx
@@ -24,7 +24,7 @@ export default function BootstrapNavigation() {
           Cards
         </h2>
         <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="images/Course2.png" />
+          <Card.Img variant="top" src="images/stacked.jpg" />
           <Card.Body>
             <Card.Title>Stacking Starship</Card.Title>
             <Card.Text>
